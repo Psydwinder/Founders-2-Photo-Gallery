cfa6a0a8139010cca6f241dca0bf92522b0527b8
Psydwinder <101563800+Psydwinder@users.noreply.github.com>
2022-07-04 18:06:17 +0100

    Added files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+const readMoreBtn1 = document.querySelector(".btn1");
+const desc = document.querySelector(".description1");
+
+readMoreBtn1.addEventListener("click", (e) => {
+  desc.classList.toggle("show-more");
+  if (readMoreBtn1.innerText === "+") {
+    readMoreBtn1.innerText = "-";
+  } else {
+    readMoreBtn1.innerText = "+";
+  }
+});
+
+const readMoreBtn2 = document.querySelector(".btn2");
+const desc2 = document.querySelector(".description2");
+
+readMoreBtn2.addEventListener("click", (e) => {
+  desc2.classList.toggle("show-more");
+  if (readMoreBtn2.innerText === "+") {
+    readMoreBtn2.innerText = "-";
+  } else {
+    readMoreBtn2.innerText = "+";
+  }
+});
+
+const readMoreBtn3 = document.querySelector(".btn3");
+const desc3 = document.querySelector(".description3");
+
+readMoreBtn3.addEventListener("click", (e) => {
+  desc3.classList.toggle("show-more");
+  if (readMoreBtn3.innerText === "+") {
+    readMoreBtn3.innerText = "-";
+  } else {
+    readMoreBtn3.innerText = "+";
+  }
+});
+
+const readMoreBtn4 = document.querySelector(".btn4");
+const desc4 = document.querySelector(".description4");
+
+readMoreBtn4.addEventListener("click", (e) => {
+  desc4.classList.toggle("show-more");
+  if (readMoreBtn4.innerText === "+") {
+    readMoreBtn4.innerText = "-";
+  } else {
+    readMoreBtn4.innerText = "+";
+  }
+});
+
+const readMoreBtn5 = document.querySelector(".btn5");
+const desc5 = document.querySelector(".description5");
+
+readMoreBtn5.addEventListener("click", (e) => {
+  desc5.classList.toggle("show-more");
+  if (readMoreBtn5.innerText === "+") {
+    readMoreBtn5.innerText = "-";
+  } else {
+    readMoreBtn5.innerText = "+";
+  }
+});
+
+const readMoreBtn6 = document.querySelector(".btn6");
+const desc6 = document.querySelector(".description6");
+
+readMoreBtn6.addEventListener("click", (e) => {
+  desc6.classList.toggle("show-more");
+  if (readMoreBtn6.innerText === "+") {
+    readMoreBtn6.innerText = "-";
+  } else {
+    readMoreBtn6.innerText = "+";
+  }
+});
